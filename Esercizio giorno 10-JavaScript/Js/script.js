83bc1aeab1298e27dfd83c6ad1a4ea47184c6334
Giulio d'Alfonso <giulius94@MBP-di-Giulio.station>
2023-11-09 12:54:44 +0100

    10

--- Esercizio giorno 10-JavaScript/Js/script.js
+++ Esercizio giorno 10-JavaScript/Js/script.js
@@ -121,31 +121,117 @@
  
        /* ESERCIZIO 11
         Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
-        La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
+        La tabella avrà 5 elementi e questa struttura: 
+        immagine, nome prodotto, quantità, prezzo
      */
  
-       const generateTable = function () {}
+       const generateTable = function () {
+        let divtable = document.querySelector('#tableArea') //selezionare la radice
+        let table = document.createElement('table');
+        table.style.border = '1px solid black'
+        let tr = document.createElement('tr');
+        let th1 = document.createElement('th');
+        th1.innerText = 'Immagine'
+        let th2 = document.createElement('th');
+        th2.innerText = 'Nome prodotto'
+        let th3 = document.createElement('th');
+        th3.innerText = 'Quantità'
+        let th4 = document.createElement('th');
+        th4.innerText = 'Prezzo'
+
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
+        tr.appendChild(th4);
+
+        table.appendChild(tr);
+        divtable.appendChild(table);
+
+         //let prodotti = ['iphone', 'samsung','motorola']
+
+        for (let i = 0; i < 5; i++) {
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            td1.innerHtml = "<img src = https://img.fruugo.com/product/6/90/535273906_max.jpg heigth = 100px />"
+            tr.appendChild(td1)
+            let td2 = document.createElement('td');
+            td2.innerText = 'pallone'
+            tr.appendChild(td2)
+            let td3 = document.createElement('td');
+            td3.innerText = '12'
+            tr.appendChild(td3)
+            let td4 = document.createElement('td');
+            td4.innerText = '20$'
+            tr.appendChild(td4)
+            table.appendChild(tr)
+        }
+
+       }
+
+       generateTable()
  
        /* ESERCIZIO 12
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
      */
  
-       const addRow = function () {}
+       const addRow = function (img, nome, qty, prezzo) {
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        td1.innerHTML = "<img src = https://img.fruugo.com/product/6/90/535273906_max.jpg height = 100px />"
+        tr.appendChild(td1)
+        let td2 = document.createElement('td');
+        td2.innerText = nome
+        tr.appendChild(td2)
+        let td3 = document.createElement('td');
+        td3.innerText = qty
+        tr.appendChild(td3)
+        let td4 = document.createElement('td');
+        td4.innerText = prezzo
+        tr.appendChild(td4)
+        let table = document.querySelector('#tableArea table')
+        table.appendChild(tr)
+       }
  
+       addRow('https://img.fruugo.com/product/6/90/535273906_max.jpg', 'iphone','5','350$')
+
        /* ESERCIZIO 14
-       Crea una funzione che nasconda le immagini della tabella quando eseguita
+       Crea una funzione che nasconda le immagini 
+       della tabella quando eseguita
      */
+
+       
  
-       const hideAllImages = function () {}
- 
+       /*const hideAllImages = function () {
+            const images = document.querySelectorAll('#tableArea table img')
+            images.forEach(img => img.style.display = 'none')
+
+       }
+       hideAllImages()
        /* EXTRA ESERCIZIO 15
        Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
      */
  
-       const changeColorWithRandom = function () {}
- 
+       const changeColorWithRandom = function () {
+        let ele = document.querySelector('#changeMyColor')
+        let red = Math.round(Math.random()*255)
+        let green = Math.round(Math.random()*255)
+        let blue = Math.round(Math.random()*255)
+        ele.style.color = `rgb(${red},${green}, ${blue})`;
+
+       }
        /* EXTRA ESERCIZIO 16
        Crea una funzione che elimini le vocali da ogni elemento testuale della pagina (puoi aiutarti con i nuovi metodi degli array di ES6)
      */
  
-       const deleteVowels = function () {}
+       const deleteVowels = function () {
+        let all = document.querySelectorAll("h1, h2, h3, p, a, li, th, td")
+        all.forEach(node => {
+            let txt = [...node.innerText].filter(char => {
+                char = char.toLowerCase();
+                return char !== "a" && char !== "e" && char !== "i" && char !== "o" && char !== "u" && char !== "y" 
+            }).join("")
+            console.log(txt)
+        })
+       }
+
+       deleteVowels()
