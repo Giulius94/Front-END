67171f6151e8f2b1eddd7d71ffebec76c7a3ffa3
Giulius94 <giuliuscb@protonmail.com>
2023-12-11 15:43:42 +0100

    esercizio giorno 25

---
+++ Esercizio giorno 25-Ajax-Fetch/assets/js/script.js
@@ -0,0 +1,69 @@
+// Example https://www.pexels.com/
+
+loadImmagini('gatto', '2')
+
+function loadImmagini(query, numeroPagina) {
+
+let campoRicerca = query;
+let pagNumber = 1
+if(numeroPagina === undefined) {
+  let pagNumber = 1;
+}else {
+  let pagNumber = +numeroPagina;
+};
+
+let UrlPexel = `https://api.pexels.com/v1/search?query=${campoRicerca}&per_page=9&page=${pagNumber}`;
+/* console.log(UrlPexel); */
+fetch(UrlPexel, {
+        method: 'GET', 
+        headers: {
+            Authorization: 'HRs3PGPbe2l9yTuPdRaS9jmZhP8uGcspBC2fPWVfT5awlh4TZX4Ccl4m'
+        }
+    })
+    .then(response => response.json())
+    .then (json => {
+      let divPadre = document.querySelector('#padreCard')
+      json.photos.forEach(element => {
+        let divCol = document.createElement('div');
+        divCol.className = 'cardsingola col-md-4 d-flex align-items-stretch';
+        divCol.innerHTML = `<div class="card mb-4 shadow-sm">
+        <img src="${element.src.large}" alt="${element.alt}">
+        <div class="card-body">
+          <p class="card-text">
+            ${element.alt}
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-success text-white btn-outline-secondary">
+                View
+              </button>
+              <button type="button" class="btn btn-sm btn-danger text-white btn-outline-secondary">
+                Hide
+              </button>
+            </div>
+            <small class="text-muted">${element.id}</small>
+          </div>
+        </div>
+      </div>`
+
+      divPadre.appendChild(divCol)
+
+      });
+    })
+
+
+    /* .then(json => {
+        let ul = document.querySelector('#photo');
+        json.photos.forEach(photo => {
+             //photo.src.small
+            let li = document.createElement('li');
+            li.className = 'list-group-item';
+            let img = document.createElement('img');
+            img.src = photo.src.small;
+            img.alt = photo.alt;
+            li.appendChild(img);
+            ul.appendChild(li);
+        })
+    })
+    .catch(error => console.log(error)) */
+  }
